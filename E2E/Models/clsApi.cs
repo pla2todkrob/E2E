a0d988daaea8e2095cc8db75d5b65c1546bc6a95
Nopparat Thongnuepad <Nopparat@thaiparker.co.th>
2022-08-03 18:26:50 +0700

    create api controller

---
+++ E2E/Models/clsApi.cs
@@ -0,0 +1,27 @@
+﻿using E2E.Models.Tables;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace E2E.Models
+{
+    public class clsApi
+    {
+        public bool isSuccess { get; set; }
+        public string Message { get; set; }
+        public object Value { get; set; }
+        public dynamic ValueClass { get; set; }
+        public clsApi()
+        {
+            isSuccess = new bool();
+        }
+    }
+
+    public class responseUser
+    {
+        public Users Users { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
