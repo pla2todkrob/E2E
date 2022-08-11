850eeaeb26b53271de8ab13e1345d7d7da1a3658
Somboonlap Kamsan <Somboonlap@thaiparker.co.th>
2022-08-11 17:04:33 +0700

    update manage page

---
+++ E2E/Models/Views/clsDocuments.cs
@@ -0,0 +1,20 @@
+﻿using E2E.Models.Tables;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace E2E.Models.Views
+{
+    public class clsDocuments
+    {
+        public Master_Documents Master_Documents { get; set; }
+        public IEnumerable<Master_DocumentVersions> Master_DocumentVersions { get; set; }
+
+        public clsDocuments()
+        {
+            Master_Documents = new Master_Documents();
+            Master_DocumentVersions = new List<Master_DocumentVersions>();
+        }
+    }
+}
