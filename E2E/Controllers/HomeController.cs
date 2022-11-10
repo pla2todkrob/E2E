644cc3d7315ccfc7dce0d57f10a992fe38c01c68
Nopparat Thongnuepad <Nopparat@thaiparker.co.th>
2022-11-10 17:26:24 +0700

    update

--- E2E/Controllers/HomeController.cs
+++ E2E/Controllers/HomeController.cs
@@ -11,38 +11,7 @@ namespace E2E.Controllers
     [AllowAnonymous]
     public class HomeController : Controller
     {
-        private clsContext db = new clsContext();
-
-        public ActionResult Index()
-        {
-            clsHome clsHome = new clsHome();
-            clsHome.Topics = db.Topics.Where(w => w.Topic_Pin).OrderBy(o => o.Create).ToList();
-            clsHome.Topics.AddRange(db.Topics.Where(w => !w.Topic_Pin).Take(10).OrderByDescending(o => o.Create).ToList());
-            clsHome.EForms = db.EForms.Take(10).OrderByDescending(o => o.Create).ToList();
-            return View(clsHome);
-        }
-
-        public ActionResult Manual_Table()
-        {
-            List<Manuals> system_Manuals = new List<Manuals>();
-            List<Guid> List_Language = db.System_Language.Select(s => s.Language_Id).ToList();
-            List<Guid> List_ManualType = db.System_ManualType.Select(s => s.Manual_Type_Id).ToList();
-
-            foreach (var item1 in List_Language)
-            {
-                foreach (var item2 in List_ManualType)
-                {
-                    var CHK = db.Manuals.Where(w => w.Manual_Type_Id == item2 & w.Language_Id == item1).OrderByDescending(o => o.Create).FirstOrDefault();
-
-                    if (CHK != null)
-                    {
-                        system_Manuals.Add(CHK);
-                    }
-                }
-            }
-
-            return View(system_Manuals);
-        }
+        private readonly clsContext db = new clsContext();
 
         public ActionResult ChangeFileUrl()
         {
@@ -198,6 +167,40 @@ namespace E2E.Controllers
                 throw ex;
             }
             return View();
+        }
+
+        public ActionResult Index()
+        {
+            clsHome clsHome = new clsHome()
+            {
+                Topics = db.Topics.Where(w => w.Topic_Pin).OrderBy(o => o.Create).ToList(),
+                EForms = db.EForms.Take(10).OrderByDescending(o => o.Create).ToList(),
+            };
+            clsHome.Topics.AddRange(db.Topics.Where(w => !w.Topic_Pin).Take(10).OrderByDescending(o => o.Create).ToList());
+
+            return View(clsHome);
+        }
+
+        public ActionResult Manual_Table()
+        {
+            List<Manuals> system_Manuals = new List<Manuals>();
+            List<Guid> List_Language = db.System_Language.Select(s => s.Language_Id).ToList();
+            List<Guid> List_ManualType = db.System_ManualType.Select(s => s.Manual_Type_Id).ToList();
+
+            foreach (var item1 in List_Language)
+            {
+                foreach (var item2 in List_ManualType)
+                {
+                    var CHK = db.Manuals.Where(w => w.Manual_Type_Id == item2 & w.Language_Id == item1).OrderByDescending(o => o.Create).FirstOrDefault();
+
+                    if (CHK != null)
+                    {
+                        system_Manuals.Add(CHK);
+                    }
+                }
+            }
+
+            return View(system_Manuals);
         }
     }
 }
